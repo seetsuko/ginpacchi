4eb363dcd69f180855ffe54709b045700c2268a3
seetsuko <radgjms@gmail.com>
2023-08-27 01:37:04 +0900

    勤務時間編集ページを作成

---
+++ src/pages/AttendancEdit.tsx
@@ -0,0 +1,11 @@
+import { PageTitle } from "@/components/PageTitle";
+
+const AttendancEdit = () => {
+    return (
+        <div>
+            <PageTitle pageTitle="勤務時間修正" />
+        </div>
+    );
+}
+
+export default AttendancEdit;
