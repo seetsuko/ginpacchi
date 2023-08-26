99e07fc57f252dcc92cf2cd09b2153fcb6fbb946
seetsuko <radgjms@gmail.com>
2023-08-27 01:38:19 +0900

    スタッフ新規登録ページを作成

---
+++ src/pages/StaffRegister.tsx
@@ -0,0 +1,11 @@
+import { PageTitle } from "@/components/PageTitle";
+
+const StaffRegister = () => {
+    return (
+        <div>
+            <PageTitle pageTitle="スタッフ新規登録" />
+        </div>
+    );
+}
+
+export default StaffRegister;
