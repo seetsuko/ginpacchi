962e46c39651868a1c4812d4ee2edc624e89ccb1
seetsuko <radgjms@gmail.com>
2023-08-26 14:34:03 +0900

    メインの日付とテキストのコンポーネントを作成

---
+++ src/components/DateAndMainText.tsx
@@ -0,0 +1,37 @@
+import { Dayjs } from "dayjs";
+import { styled } from "styled-components";
+
+type Props = {
+    date: Dayjs
+    mainText: string
+}
+
+const DateAndTextStyle = styled.div`
+    text-align: center;
+    width: 100vw;
+    margin-top: 50px;
+    h1{
+        margin: 10px auto;
+        width: 340px;
+        border-bottom: 1px solid #7b7b7b;
+        font-size: 68px;
+    }
+    p{
+        color: #444;
+        font-size: 30px;
+    }
+`
+
+export const DateAndMainText = ({date, mainText}: Props) => {
+
+    return (
+        <DateAndTextStyle>
+            <div>
+            <p>{date.toString()}</p>
+            </div>
+            <div>
+            <h1>{mainText}</h1>
+            </div>
+        </DateAndTextStyle>
+    );
+}
