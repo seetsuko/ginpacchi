b7e3882f63ed5791a74cb2df7cf36f031e301b05
seetsuko <radgjms@gmail.com>
2023-08-27 01:38:05 +0900

    ファイル名が間違っていたので修正した

---
+++ src/pages/StaffManege.tsx
@@ -0,0 +1,29 @@
+
+import { MembersAndSearch } from "@/components/MembersAndSearch";
+import { PageTitle } from "@/components/PageTitle";
+import Link from "next/link";
+import { styled } from "styled-components";
+
+const StaffAddLinkStyle = styled.div`
+    text-align: center;
+    margin: 60px auto;
+    font-size: 33px;
+    color: #0059f3;
+    border-bottom: 1px solid #0059f3;
+    width: 250px;
+`
+
+
+const StaffManege = () => {
+    return (
+        <div>
+            <PageTitle pageTitle="スタッフ管理"/>
+            <StaffAddLinkStyle>
+                <Link href="/">スタッフを追加</Link>
+            </StaffAddLinkStyle>
+            <MembersAndSearch />
+        </div>
+    );
+}
+
+export default StaffManege;
