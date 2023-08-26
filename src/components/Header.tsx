73575b7c5d5f0407a727ee3ef8688eb6c835fb7e
seetsuko <radgjms@gmail.com>
2023-08-26 14:34:28 +0900

    ヘッダーのスタイルを変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,16 @@
-"use client"
-
 import styled from "styled-components";
 import { AiOutlineMenu } from "react-icons/ai";
 
 const HeaderStyle = styled.div`
     background-color: #89bcff;
-    height: 100px;
-    padding: 27px;
+    height: 120px;
+    padding: 30px;
 `
 
 export const Header = () => {
     return (
         <HeaderStyle>
-            <AiOutlineMenu size={40}/>
+            <AiOutlineMenu size={50}/>
         </HeaderStyle>
     );
 }
