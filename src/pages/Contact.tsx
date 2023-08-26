0c882494b325c172bd8db3c1919344f49aa079fe
seetsuko <radgjms@gmail.com>
2023-08-27 01:37:20 +0900

    お問合せページを作成

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,11 @@
+import { PageTitle } from "@/components/PageTitle";
+
+const Contact = () => {
+    return (
+        <div>
+            <PageTitle pageTitle="お問い合わせ" />
+        </div>
+    );
+}
+
+export default Contact;
