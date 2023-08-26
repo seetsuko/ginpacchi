18e0e9a021b4756977150c2db474e64625a46dfb
seetsuko <radgjms@gmail.com>
2023-08-27 01:37:37 +0900

    スタッフ情報編集ページを作成

---
+++ src/pages/StaffEdit.tsx
@@ -0,0 +1,11 @@
+import { PageTitle } from "@/components/PageTitle";
+
+const StaffEdit = () => {
+    return (
+        <div>
+            <PageTitle pageTitle="スタッフ情報編集"/>
+        </div>
+    );
+}
+
+export default StaffEdit;
