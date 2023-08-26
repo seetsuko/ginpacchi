138f441bc61cfeff98c3e59a6b5224b71b321bb5
seetsuko <radgjms@gmail.com>
2023-08-26 14:35:27 +0900

    勤務時間管理画面ページを作成

---
+++ src/pages/AttendanceManege.tsx
@@ -0,0 +1,22 @@
+// 勤怠管理画面
+
+const AttendanceManegeStyle = styled.div`
+    
+`
+
+import { DateAndMainText } from "@/components/DateAndMainText";
+import dayjs from "dayjs";
+import { styled } from "styled-components";
+
+const today = dayjs()
+const staffName = "tatata"
+
+const AttendanceManege = () => {
+    return (
+        <AttendanceManegeStyle>
+            <DateAndMainText date={today} mainText={staffName}/>
+        </AttendanceManegeStyle>
+    );
+}
+
+export default AttendanceManege;
