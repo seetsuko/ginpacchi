7f652a147dd8c6a089cf018c9d72b4e26830690a
seetsuko <radgjms@gmail.com>
2023-09-04 14:48:06 +0900

    inputのstorybookを作成

---
+++ src/components/Input/input.stories.ts
@@ -0,0 +1,40 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Input from ".";
+
+const meta: Meta<typeof Input> = {
+  component: Input,
+  title: "Input",
+  argTypes: {
+    variant: {
+      control: { type: "radio" },
+      options: [ "textInput", "contactTextarea", "memoTextarea" ]
+    },
+    label: {
+      control: { type: "text" }
+    },
+    placeholder: {
+      control: { type: "text" }
+    }
+  },
+  tags: ["autodocs"]
+}
+
+export default meta
+
+type story = StoryObj<typeof meta>
+
+export const TextInput: story = {
+  args: {
+    variant: "textInput"
+  }
+}
+export const ContactTextarea: story = {
+  args: {
+    variant: "contactTextarea"
+  }
+}
+export const MemoTextarea: story = {
+  args: {
+    variant: "memoTextarea"
+  }
+}
